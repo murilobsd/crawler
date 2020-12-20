89ef11138d74c4aba1e4b492f6b9ada408b9a73a
mbsd <mbsd@m0x.ru>
2020-12-19 23:21:10 -0300

    test(request): test url request property

--- src/main.rs
+++ src/main.rs
@@ -12,3 +12,15 @@ fn main() {
     let url = "http://httpbin.org/ip";
     let _ = Request::new(url);
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn new_request_url_ok() {
+        let url = "http://httpbin.org/ip";
+        let req = Request::new(url);
+        assert_eq!(url, req.url);
+    }
+}
