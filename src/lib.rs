9d504d4ea07624b3f8577dfa7bb31554818a584f
mbsd <mbsd@m0x.ru>
2020-12-23 23:01:44 -0300

    refactoring spider

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,23 @@
-pub mod http;
-pub mod site;
-pub mod queue;
-pub mod spider;
+pub mod spider {
+    /// This module is responsible for parsing the response.
+    pub struct Spider<'a> {
+        pub name: &'a str,
+    }
+
+    impl<'a> Spider<'a> {
+        pub fn new(name: &'a str) -> Spider<'a> {
+            Spider { name }
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::Spider;
+
+        #[test]
+        fn spider_new_test() {
+            let sp = Spider::new("rakun");
+            assert_eq!("rakun", sp.name);
+        }
+    }
+}
