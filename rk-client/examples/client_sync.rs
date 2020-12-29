5300f49a2c496e5e07eea913397caa1f56d95f78
mbsd <mbsd@m0x.ru>
2020-12-29 14:28:15 -0300

    example how request sync

--- rk-client/examples/client_sync.rs
+++ rk-client/examples/client_sync.rs
@@ -11,4 +11,11 @@
 // WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
 // ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 // OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-fn main() {}
+use rk_client::client::RakunClient;
+
+fn main() {
+    let client = RakunClient::new();
+
+    #[warn(unused_must_use)]
+    client.execute();
+}
