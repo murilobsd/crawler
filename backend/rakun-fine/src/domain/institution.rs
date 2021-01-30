ecaa77bda9c20709ff7cd8686cfcfcd7a7ae83ab
mbsd <mbsd@m0x.ru>
2021-01-30 16:45:47 -0300

    test(fine): create institution with and without id

--- backend/rakun-fine/src/domain/institution.rs
+++ backend/rakun-fine/src/domain/institution.rs
@@ -1,46 +1,78 @@
-use crate::domain::Fine;
+use std::fmt;
 
 /// The identification of institution
 #[derive(Debug, Eq, PartialEq, Clone)]
 pub struct InstitutionId(pub String);
 
-/// Institution generating the fine
+impl fmt::Display for InstitutionId {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+/// A struct which represents institution generating the fine.
+///
+/// Institution store data of Detrans, DER's.
 #[derive(Debug, Eq, PartialEq, Clone)]
 pub struct Institution {
-    pub active: bool,
-    pub captcha: bool,
     pub city: Option<String>,
     pub id: Option<InstitutionId>,
-    pub name: String,
+    pub name: &'static str,
     pub uf: Option<String>,
     pub url: Option<String>,
-    pub fines: Option<Vec<Fine>>,
+}
+
+impl fmt::Display for Institution {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match &self.url {
+            Some(u) => write!(f, "Institution: {} - {}", self.name, u),
+            None => write!(f, "Institution: {}", self.name),
+        }
+    }
 }
 
 impl Institution {
-    pub fn new(name: String) -> Self {
+    /// Returns a new institution
+    pub fn new(name: &'static str) -> Self {
         Self {
-            active: false,
             city: None,
-            description: None,
             id: None,
             name,
             uf: None,
             url: None,
-            fines: None,
         }
     }
 
-    pub fn new_with_id(id: Option<InstitutionId>, name: String) -> Self {
+    /// Returns a new institution with id
+    pub fn new_with_id(id: Option<InstitutionId>, name: &'static str) -> Self {
         Self {
-            active: false,
             city: None,
-            description: None,
             id,
             name,
             uf: None,
             url: None,
-            fines: None,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn institution_without_id() {
+        let institution = Institution::new("DER - SP");
+        assert_eq!(institution.name, "DER - SP");
+    }
+
+    #[test]
+    fn institution_with_id() {
+        let institution_id = InstitutionId("1".to_string());
+        let institution = Institution::new_with_id(Option::from(institution_id), "DER - SP");
+        assert_eq!(institution.name, "DER - SP");
+        match institution.id {
+            Some(v) => assert_eq!(v.0, "1".to_string()),
+            None => panic!("institution not has id"),
         }
     }
 }
