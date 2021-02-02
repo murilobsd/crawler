ea766de190057bc78b3119a2279f3ee38b3a01be
mbsd <mbsd@m0x.ru>
2021-02-02 01:06:19 -0300

    feat: init domain

---
+++ rakun-application/src/domain/car.rs
@@ -0,0 +1,12 @@
+/// Car
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub struct Car {}
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn hi_test() {
+        assert_eq!(2, 1 + 1);
+    }
+}
---
+++ rakun-application/src/domain/fine.rs
@@ -0,0 +1,17 @@
+/// Fine
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct Fine {
+    /// The description ...
+    pub description: String,
+    /// The ait ...
+    pub ait: u32,
+}
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn hi_test() {
+        assert_eq!(2, 1 + 1);
+    }
+}
